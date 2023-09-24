af9cb1df4b9c7276633cf339f5ae04f957c263e1
ignacio <ign.garcia61@gmail.com>
2023-09-24 14:54:12 -0300

    refac app/server

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,7 @@
 import 'express-async-errors';
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import mongoose from './config/db/';
-import routers from './routes';
+import app from './server';
 
-const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running at ${PORT}.`));
 
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, 'connection error:'));
-db.once("open", function () {
-  console.log('database connection succeeded');
-});
-
-app.use(morgan('tiny'));
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-
-
-app.use("/api", routers);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello World");
-})
- 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).send(error.message);
-})
- 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import mongoose from './config/db/';
+import routers from './routes';
+
 dotenv.config();
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, 'connection error:'));
+db.once("open", function () {
+  console.log('database connection succeeded');
+});
  
-const PORT = parseInt(`${process.env.PORT || 3000}`);
- 
-import app from './app';
- 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}.`));
+const app = express();
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use("/api", routers);
+
+export default app;
