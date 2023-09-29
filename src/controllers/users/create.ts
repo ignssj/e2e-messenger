2c178d3b0936aa9aa89570178db68c58ee73a116
ignacio <ign.garcia61@gmail.com>
2023-09-29 14:09:05 -0300

    hashing password on user creation

--- src/controllers/users/create.ts
+++ src/controllers/users/create.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { validation } from '../../middleware/validation';
 import {StatusCodes} from 'http-status-codes';
 import { User } from '../../models/users';
+import bcrypt from 'bcrypt';
 import * as yup from 'yup';
 
 interface IUser {
@@ -18,7 +19,13 @@ export const createValidation = validation((getSchema) => ({
 
 export const createUser = async (req: Request<{},{},IUser>, res: Response) => {
     const {username, password} = req.body;
-    const user = User.build({username, password});
-    const response = await user.save();
-    return res.status(StatusCodes.CREATED).send(response);
+    try{
+        bcrypt.hash(password, 15, async function(err, hash) {
+            const user = User.build({username, password: hash});
+            const response = await user.save();
+            return res.status(StatusCodes.CREATED).send(response);
+        });
+    }catch(err){
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Interal error')
+    }
 };
