cec565dacbf632b20ba0c88aec452a7d85df4b1c
ignacio <ign.garcia61@gmail.com>
2023-10-02 13:22:18 -0300

    messages router added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import users from './users/';
 import auth from './auth';
+import messages from './messages';
 const router = Router();
 
 router.use('/auth', auth);
 router.use('/users', users);
+router.use('/messages', messages);
 
 export default router;
